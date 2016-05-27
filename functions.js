26ab36642d38001f1214ad82ed1ee35b293471d1
Zuiiey <lindasuihko@gmail.com>
2016-05-27 14:32:59 +0200

    Visa divar försök 1
    Lyckas visa varje label i todoData

--- functions.js
+++ functions.js
@@ -1,11 +1,55 @@
-{
-  displayComplete: true;
+document.body.onload = addElement;
+
+
+var todoData = {
+  displayComplete: true,
   items: [
     {
-      label: "Skrubba golv", completed: true;
+      label: "Skrubba golv",
+      completed: true
     },
     {
-      label: "Töm sopor", completed: true;
+      label: "Töm sopor",
+      completed: true
+    },
+    {
+      label: "Rengör kaffemaskinen",
+      completed: true
     }
   ]
+};
+
+//console.log(todoData.items[1].label);
+console.log(todoData.items.length);
+
+function addElement () {
+
+   /*for (var items in todoData) {
+     var i = 0;
+      var newDiv = document.createElement("div");
+      var newContent = document.createTextNode("label");
+      newDiv.appendChild(newContent);
+      console.log(items + i);
+      i++;
+  } */
+
+  for (i = 0; i < todoData.items.length; i++) {
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode(todoData.items[i].label);
+    newDiv.appendChild(newContent);
+
+    var currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
+
+/*
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode("Hi there and greetings!");
+    newDiv.appendChild(newContent); //add the text node to the newly created div.
+
+    // add the newly created element and its content into the DOM
+    var currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+    */
+
 }
